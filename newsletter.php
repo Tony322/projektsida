78f4f4adb065e9b909569033a2da83dddfc08efe
Tony <tony_wennberg@msn.com>
2015-12-17 15:11:33 +0100

    Fixade global header och logo

--- newsletter.php
+++ newsletter.php
@@ -18,8 +18,16 @@ clear:both;
   <body>
     <div id="container">
 	
-		<header id="logoheader">Logo</header>
-		<header id="header">Page Header</header>
+		<header id="logoheader">
+			<?php
+				include('headerlogo.html');
+			?>
+		</header>
+		<header id="header">
+			<?php
+				include('header.html');
+			?>
+		</header>
 
 		  		  
         <nav id="nav">
@@ -34,7 +42,7 @@ clear:both;
 		<nav id="mainnav">
 			<ul id="breadcrumbs">
 				<li><a href="index.php">Hem /</a></li>
-				<li><a href="index.php">Nyhetsbrev</a></li>
+				<li><a href="newsletter.php">Nyhetsbrev</a></li>
 			</ul>
 		
 		
@@ -44,6 +52,8 @@ clear:both;
         <main>
           <article class="newsForm">
 			<?php
+			
+			/*Kolla om formen är ifylld och om den är det skriv ut tack för blabla ANNARS skriv ut formen.*/
 			if(isset($_POST['email']) and isset($_POST['wantNews']) and isset($_POST['name']) and isset($_POST['lastName'])){
 				if($_POST['email'] != "" and $_POST['wantNews'] == "WANT") {
 					print('<h2>Tack för din registrering!</h2>');
