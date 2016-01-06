62633f58b1dfbd626c281a9c0e760412bcee96ba
GoranMards <goran.mards@gmail.com>
2016-01-06 11:26:48 +0100

    updaterad about

--- about.php
+++ about.php
@@ -1,27 +1,103 @@
-﻿<h1>Om Speldalen</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+	<meta name="robots" content="noindex, nofollow"/>
+    <title>Speldalen - Nyhetsbrev.</title>
+    <style type="text/css">
+body {
+background-color:#FFFFFF;
+color:#000000;    
+}
+.clearfix {
+clear:both;
+}
+</style>
+    <link rel="stylesheet" style="text/css" href="hemstyle.css" />
+  </head>
+ 
+  <body>
+    <div id="container">
+	
+		<header id="logoheader">
+			<?php
+				include('headerlogo.html');
+			?>
+		</header>
+		<header id="header">
+			<?php
+				include('header.html');
+			?>
+		</header>
 
-<p>Speldalen är ett företag lokaliserat i Borlänge, vi säljer spel till konsoler.
-Vi anser att du som konsument inte ska behöva leta efter spel, tag kontakt med oss så hjälper vi dig att finna det du söker.
+		  		  
+        <nav id="nav">
+		<?php
+		include('nav.html');
+		?>
+		
+		</nav>
+		<nav id="mainnav">
+			<ul id="breadcrumbs">
+				<li><a href="index.php">Hem /</a></li>
+				<li><a href="newsletter.php">Nyhetsbrev</a></li>
+			</ul>
+		
+		
+		</nav>
+		
+		
+        <main>
+		<article class="about">
+         <h1>Om Speldalen</h1>
 
-
-
+<p>Speldalen är ett företag lokaliserat i Borlänge, vi säljer spel till konsoler.<br>
+Vi anser att du som konsument inte ska behöva leta efter spel.<br>
+Tag kontakt med oss så hjälper vi dig att finna det du söker.
 </p>
 
-<script src="http://maps.googleapis.com/maps/api/js"></script>
-<script>
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
+<div
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2337.367302365552!2d15.410442422032398!3d60.48674594492901!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x749c3742378bbfa7!2sH%C3%B6gskolan+Dalarna!5e0!3m2!1ssv!2sus!4v1452075562705" width="400" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
 
-
-
-<body>
-<div id="googleMap" style="width:500px;height:380px;"></div>
-</body>
+		 <p> Speldalen AB
+		 <br>
+			Röda vägen 3
+			<br>
+			781 70 Borlänge,Sverige
+			<br>
+			Tel: 0243-111 222
+			<br>
+			Maila oss
+<a href="mailto:info@speldalen.se?Subject=Hej!" target="_top">Skicka Mail</a>
+</p>
+			
+		 
+		 </article>
+        </main>
+		
+        <footer id="footer">
+		<?php
+		include('footer.html');
+		?>
+		</footer>
+    </div>
+	
+	
+    <!-- W3C logos for valid HTML5 and CSS3 -->
+    <div class="clearfix"></div>
+    <div>
+      <p>
+        <a href="http://validator.w3.org/check?uri=referer">
+          <img src="http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png" alt="Valid HTML5" height="50" width="50"
+          style="border:0;" />
+        </a>
+      </p>
+      <p>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+          <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+          alt="Valid CSS!" />
+        </a>
+      </p>
+    </div>
+  </body>
+</html>
