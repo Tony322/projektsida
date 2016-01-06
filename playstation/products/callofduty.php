5fc55fe5885a7c4021cc63ae84e20d2354e25613
rubankh <rubankh@hotmail.com>
2016-01-06 14:25:07 +0100

    addat ps sidan

---
+++ playstation/products/callofduty.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+	<meta name="robots" content="noindex, nofollow"/>
+    <title>Speldalen - Nintendo - Mariokart</title>
+    <style type="text/css">
+body {
+background-color:#FFFFFF;
+color:#000000;    
+}
+.clearfix {
+clear:both;
+}
+</style>
+    <link rel="stylesheet" style="text/css" href="../../hemstyle.css" />
+  </head>
+ 
+  <body>
+    <div id="container">
+	
+		<header id="logoheader">
+			<?php
+				include('headerlogo.html');
+			?>
+		</header>
+		<header id="header">
+			<?php
+				include('header.html');
+			?>
+		</header>
+
+		<nav id="nav">
+		<?php
+		include('nav.html');
+		?>
+		</nav>  		  
+        
+		<nav id="mainnav">
+		<ul id="breadcrumbs">
+			<li><a href="../../index.php">Hem /</a></li>
+			<li><a href="../../konsoler.php">Konsoler /</a></li>
+			<li><a href="../index.php">Nintendo</a></li>
+		</ul>
+		
+		
+		</nav>
+		
+		
+        <main>
+          <article class="product">
+			<h2>Mariokart 8</h2>
+			<img src="../../bilder/nintendo/mariokart.jpg" alt="Mariokart 8"/>
+			<ul>
+				<li>Genere: Racing</li>
+				<li>Multiplayer: Ja</li>
+				<li>Releasedatum: 2014</li>
+				<li>I lager: Ja</li>
+			
+			</ul>
+					
+		  <p>Marioklart i all ära, tävla och ha kul med familjen!. <br>
+		  Upp till 4 spelare lokalt samtidigt eller online mot 8 andra!</p>
+		  
+		  <h4>Pris: 499:-</h4>
+		  <button type="submit">Köp</button>
+		  
+		  
+          </article>
+        </main>
+		
+        <footer id="footer">
+			<?php
+			include('footer.html');
+			?>
+		</footer>
+    </div>
+	
+	
+    <!-- W3C logos for valid HTML5 and CSS3 -->
+    <div class="clearfix"></div>
+    <div>
+      <p>
+        <a href="http://validator.w3.org/check?uri=referer">
+          <img src="http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png" alt="Valid HTML5" height="50" width="50"
+          style="border:0;" />
+        </a>
+      </p>
+      <p>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+          <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+          alt="Valid CSS!" />
+        </a>
+      </p>
+    </div>
+  </body>
+</html>
