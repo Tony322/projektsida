d40b13a8aca117f19bbef5014718f2be811723d4
rubankh <rubankh@hotmail.com>
2016-01-09 09:21:00 +0100

    xbox!

---
+++ xbox/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+	<meta name="robots" content="noindex, nofollow"/>
+	<meta name=“description” content="Speldalen - Köp senaste konsolspelen till xbox Wii U/Wii U i Borlänge till de senaste konsolerna"/>
+    <title>Speldalen - Xbox</title>
+    <style type="text/css">
+body {
+background-color:#FFFFFF;
+color:#000000;    
+}
+.clearfix {
+clear:both;
+}
+</style>
+    <link rel="stylesheet" style="text/css" href="../hemstyle.css" />
+  </head>
+ 
+  <body>
+    <div id="container">
+	
+		<header id="logoheader">
+			<?php
+				include('headerlogo.html');
+			?>
+		</header>
+		<header id="header">
+			<?php
+				include('header.html');
+			?>
+		</header>
+
+				        <nav id="nav">
+		<?php
+		include('nav.html');
+		?>
+		</nav>  		  
+        
+		<nav id="mainnav">
+			<ul id="breadcrumbs">
+				<li><a href="../index.php">Hem /</a></li>
+				<li><a href="../konsoler.php">Konsoler /</a></li>
+				<li><a href="index.php">Xbox</a></li>
+			</ul>
+		</nav>
+		
+        <main>
+          <article>
+		  
+			<h1>Xbox</h1>
+			<table class="productlisttable">
+			<tr>
+				<td>
+					<p>Grand theft auto</p>
+					<a href="products/grandtheft.php"><img src="../bilder/xbox/grandtheft.jpg" alt="Grand theft auto"/></a>
+				</td>
+				<td>
+					<p>Call of duty</p>
+					<a href="products/callofduty.php"><img src="../bilder/xbox/callofduty.jpg" alt="Call of duty"/></a>
+				</td>
+				<td>
+					<p>Minecraft</p>
+					<a href="products/minecraft.php"><img src="../bilder/xbox/minecraft.jpg" alt="Minecraft"/></a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>Nhl 2016</p>
+					<a href="products/nhl2016.php"><img src="../bilder/xbox/nhl2016.jpg" alt="Nhl 2016"/></a>
+				</td>
+				<td>
+					<p>Starwars</p>
+					<a href="products/starwars.php"><img src="../bilder/xbox/starwars.jpg" alt="Starwars"/></a>
+				</td>
+				<td>
+				</td>
+			</tr>
+			</table>
+			
+          </article>
+        </main>
+		
+        <footer id="footer">
+			<?php
+			include('footer.html');
+			?>
+		</footer>
+    </div>
+	
+	
+    <!-- W3C logos for valid HTML5 and CSS3 -->
+    <div class="clearfix"></div>
+    <div>
+      <p>
+        <a href="http://validator.w3.org/check?uri=referer">
+          <img src="http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png" alt="Valid HTML5" height="50" width="50"
+          style="border:0;" />
+        </a>
+      </p>
+      <p>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+          <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+          alt="Valid CSS!" />
+        </a>
+      </p>
+    </div>
+  </body>
+</html>
