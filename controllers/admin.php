57fe9cb04e02b2345b6a25427438569bba4e0ac2
NecirvanT <taysun_necirvan@hotmail.com>
2017-02-14 12:35:21 +0100

    login form skickar anv o lösenord

---
+++ controllers/admin.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Necirvan
+ * Date: 2017-02-09
+ * Time: 13:45
+ */
+
+include_once "./models/ProductModel.php";
+
+class admin
+
+
+{
+ public function login() {
+    include_once'./views/login.php';
+}
+
+public function addProduct($para) {
+
+}
+
+public function deleteProduct($para) {
+
+}
+
+public function updateProduct($para) {
+
+}
+
+public function authUser() {
+     $model = new ProductModel();
+
+     $username = $_POST['username'];
+     $password = $_POST['password'];
+
+     $result = $model->checkUser($username, $password);
+
+     if($result[0][0] == '1') {
+        header("Location: login.php");
+     } else {
+         $this->login();
+
+     }
+
+ }
+}
--- models/ProductModel.php
+++ models/ProductModel.php
@@ -4,9 +4,6 @@
 
 class ProductModel {
 
-    //Prefix för tabellnamn.
-    public $tablePrefix = "tony_";
-
     public function getAllGames() {
         try {
             //Tabellnamn exkl. prefix.
@@ -183,6 +180,69 @@ class ProductModel {
 
             return $games;
         } catch (PDOException $pdoexp) {
+            $pdocon = NULL;
+            throw new Exception('Databasfel!');
+        }
+    }
+
+    public function getUserById() {
+        try {
+            $tablePostfix = 'products';
+
+            $tableName = $this->tablePrefix . $tablePostfix;
+            $details = 'mysql:host=mards.se;dbname=webshop';
+            $usr = 'tony';
+            $pw = 'Vennberg';
+
+            $pdocon = new PDO($details, $usr, $pw);
+            $querystr = 'call h15tonve_getUserByUserId()';
+
+            //Förbered frågan
+            $query = $pdocon->prepare($querystr);
+
+            //Kör frågan
+            $query->execute();
+
+            //Ta emot resultat som en array.
+            $games = $query->fetchAll();
+            $pdocon = NULL;
+
+            return $games;
+        }
+        catch (PDOException $pdoexp){
+            $pdocon = NULL;
+            throw new Exception('Databasfel!');
+        }
+
+
+    }
+
+    public function checkUser($username, $password) {
+        try {
+
+            $details = 'mysql:host=mards.se;dbname=webshop';
+            $usr = 'tony';
+            $pw = 'Vennberg';
+
+            $pdocon = new PDO($details, $usr, $pw);
+            $querystr = 'call h15tonve_authUser(?, ?)';
+
+            //Förbered frågan
+            $query = $pdocon->prepare($querystr);
+
+            $query->bindValue(1, $username, PDO::PARAM_STR);
+            $query->bindValue(2, $password, PDO::PARAM_STR);
+
+            //Kör frågan
+            $query->execute();
+
+            //Ta emot resultat som en array.
+            $games = $query->fetchAll();
+            $pdocon = NULL;
+
+            return $games;
+        }
+        catch (PDOException $pdoexp){
             $pdocon = NULL;
             throw new Exception('Databasfel!');
         }
