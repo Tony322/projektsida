a66a7c984f2e4786f3e6462dd997b7f1bf189739
rubankh <rubankh@hotmail.com>
2017-02-14 13:14:49 +0100

    lagt till functioner

--- controllers/admin.php
+++ controllers/admin.php
@@ -7,47 +7,53 @@
  * Time: 13:45
  */
 
-include_once "./models/ProductModel.php";
+include_once './models/adminModel.php';
+include_once './views/ViewHelper.php';
 
-class admin
+class admin {
 
+public function addProduct($para) {
 
-{
-    public function login()
-    {
-        include_once './views/login.php';
+}
+
+public function deleteProduct($id) {
+    $model = new adminModel();
+
+    $model->deleteProduct($id);
+    $this->all();
+}
+
+    public function all() {
+        $model = new adminModel();
+        $games = $model->getAllGames();
+
+        $viewhelper = new ViewHelper();
+        $viewhelper->assign('games', $games);
+        $viewhelper->display("admin.php");
     }
 
-    public function addProduct($para)
-    {
+public function updateProduct($para) {
 
-    }
+}
 
-    public function deleteProduct($para)
-    {
+public function authUser() {
+     $model = new ProductModel();
 
-    }
+     $username = $_POST['username'];
+     $password = $_POST['password'];
 
-    public function updateProduct($para)
-    {
+     $result = $model->checkUser($username, $password);
 
-    }
+     if($result[0][0] == '1') {
+        header("Location: login.php");
+     } else {
+         $this->login();
 
-    public function authUser()
-    {
-        $model = new ProductModel();
+     }
 
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+ }
 
-        $result = $model->checkUser($username, $password);
-
-        if ($result[0][0] == '1') {
-            header("Location: ./views/login.php");
-        } else {
-            $this->login();
-
-        }
-
-    }
+ public function login() {
+    include_once'./views/login.php';
+}
 }
